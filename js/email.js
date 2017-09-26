d65c79f14e634288cdcc4472839a53dfb5a1533c
liutao3253 <1937373367@qq.com>
2017-09-27 00:13:05 +0800

    'fullpage1.0'

---
+++ js/email.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('#fullpage').fullpage({
+        'navigation': true,//圆点
+        // 'navigationPosition': 'right',//右侧圆点
+        'navigationTooltips': ['首页', '交互', '视觉', '皮肤','功能','代办邮件','联系人邮件','科技','连接易信','马上体验'],
+        'navigationColor':'#8B9FA9'
+    });
+})
